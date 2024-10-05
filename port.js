7c0342a287fd3484919c7f96ad5083ea5a5b4c5e
ragul <ragulaa098@gmail.com>
2024-10-05 08:25:19 +0530

    renponsive

---
+++ port.js
@@ -0,0 +1,9 @@
+
+function op(){
+    var open=document.querySelector(".pbarnav")
+ open.style.left="0"
+}
+function clo(){
+    var close=document.querySelector('.pbarnav')
+    close.style.left='-260px'
+}
